bfa40837fd03b0eb096b0c5b3ff89d3aad674288
Jamiya B <89568531+Miya85@users.noreply.github.com>
2024-01-11 11:48:05 -0500

    Add files via upload

---
+++ Program Template.cs
@@ -0,0 +1,48 @@
+// Bates, Jamiya Program Template, v0.0
+using system;
+
+namespace UPDATEFOREACHPROGRAM
+{
+    claas UPDATEFOREACHPROGRAM
+    {
+        static void Main(string[] arg)
+        {  
+            
+            // Declaring and assigning variables
+            // Integer -- -2,147,483,648 to 2,147,483,647
+            //Integer = 4 Bytes
+            int myInt = 5;
+        
+            // Long -- -9,223,372,036,854,775,888 to 9,223,372,036,854,775,807
+            // Long = 8 Bytes
+            Long myLong = 1928742747242904;
+
+            // Float -- Stores ip to 6-7 decimal digits.            
+            // Float = 4 Bytes
+            Float myFloat = 3.1432145F;
+
+            // Double -- Stores up to 15 decimal digits.
+            // Double = Bytes
+            double myDouble = 9.123456789102345D;
+
+            
+            // Boolean -- true / false
+            // Boolean = 1 bit
+            bool myBool = true;
+
+
+            // String
+            // 2-bytes PER CHARACTER
+            // Must be in DOUBLE QUOTES " "
+            string myString = "I Love Food";
+
+
+            // Character (Char)
+            // Single character, in SINGLE QUOTES ' '
+            // 2-bytes
+            char myChar = 'C';
+
+
+        }
+    }
+}
